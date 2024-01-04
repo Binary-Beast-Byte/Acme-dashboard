b4650f74948f41d93493d394c6e1627ed134f0bb
Ravi Neupane <ravi.neupane@vedastudios.com>
2024-01-04 15:41:29 +0545

    feat: create crud operations for invoices with server actions

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,23 +6,22 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
+
 export default async function Page({
-    searchParams,
+  searchParams,
 }: {
-    searchParams?: {
-        query?: string;
-        page?: string;
-    }
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
 }) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
-    const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
-    const totalPages = await fetchInvoicesPages(query);
-
-    console.log('query', query)
-    console.log('currentPage', currentPage)
+  console.log('query', query);
+  console.log('currentPage', currentPage);
 
   return (
     <div className="w-full">
@@ -33,7 +32,7 @@ export default async function Page({
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
