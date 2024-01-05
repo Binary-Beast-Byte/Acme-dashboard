c79228e467c0ce1dfe289715a4f718c603261d11
Ravi Neupane <ravi.neupane@vedastudios.com>
2024-01-05 09:35:35 +0545

    feat: handle error in server actions and create not found page

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,9 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <img className='w-20' src="https://static.vecteezy.com/system/resources/thumbnails/024/553/676/small/skull-wearing-crown-logo-skull-king-sticker-pastel-cute-colors-generative-ai-png.png" alt="" />
+      <p className="text-[44px] text-orange-200">Ace</p>
     </div>
   );
 }
